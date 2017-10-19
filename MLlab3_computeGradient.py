ef25c20009a4a3a78191509498ec0230209132b4
Xsolute <xsolute01@gmail.com>
2017-10-19 23:48:01 +0900

    complete MLlab3_computeGradient

--- MLlab3_computeGradient.py
+++ MLlab3_computeGradient.py
@@ -10,4 +10,22 @@ W = tf.Variable(5.)
 hypothesis = X * W
 
 # manual gradient
-gradient = tf.reduce_mean(tf.)
+gradient = tf.reduce_mean((W * X - Y) * X) * 2
+
+# cost/loss function
+cost = tf.reduce_mean(tf.square(hypothesis - Y))
+optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01)
+
+# get gradients
+gvs = optimizer.compute_gradients(cost)
+
+# apply gradients
+apply_gradients = optimizer.apply_gradients(gvs)
+
+# launch the graph in a session
+sess = tf.Session
+sess.run(tf.global_variables_initializer())
+
+for step in range(100):
+    print(step, sess.run([gradient, W, gvs]))
+    sess.run(apply_gradients)
