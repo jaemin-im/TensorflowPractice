dc283c98b45ac22630db05a53db68a8eb3d48507
Xsolute <xsolute01@gmail.com>
2017-10-27 19:46:21 +0900

    complete MLlab6-2

--- MLlab6-2.py
+++ MLlab6-2.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 # predicting animal type based on various features
-xy = np.loadtext('data-04-zoo.csv', delimiter=',', dtype=np.float32)
+xy = np.loadtxt('data-04-zoo.csv', delimiter=',', dtype=np.float32)
 x_data = xy[:, 0:-1]
 y_data = xy[:, [-1]]
 
@@ -26,3 +26,28 @@ cost_i = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=Y_one_hot
 
 cost = tf.reduce_mean(cost_i)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(cost)
+
+prediction = tf.argmax(hypothesis, 1)
+correct_prediction = tf.equal(prediction, tf.argmax(Y_one_hot, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# launch graph
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+
+    for step in range(2001):
+        sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+        if step % 100 == 0:
+            loss, acc = sess.run([cost, accuracy], feed_dict={
+                X: x_data, Y: y_data
+            })
+            print("Step: {:5}\tLoss: {:.3f}\tAcc: {:.2%}".format(
+                step, loss, acc
+            ))
+
+    # let's see if we can predict
+    pred = sess.run(prediction, feed_dict={X: x_data})
+
+    # y_data: (N, 1) = flatten => (N,  ) matches pred.shape
+    for p, y in zip(pred, y_data.flatten()):
+        print("[{}] prediction: {} True Y: {}".format(p == int(y), p, int(y)))
