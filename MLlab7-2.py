55cd254d4033ef48e48047c0053a8283fb6141c6
Xsolute <xsolute01@gmail.com>
2017-10-28 15:08:15 +0900

    complete MLlab7-2, add MNIST data

--- MLlab7-2.py
+++ MLlab7-2.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import random
+tf.set_random_seed(777) # for reproducibility
+
 from tensorflow.examples.tutorials.mnist import input_data
 # check out https://www.tensorflow.org/get_started/mnist/beginners for
 # more information about the mnist dataset
@@ -25,3 +29,43 @@ is_correct = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
 
 # calculate accuracy
 accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+# parameters
+training_epochs = 15
+batch_size = 100
+
+with tf.Session() as sess:
+    # initialize TensorFlow variables
+    sess.run(tf.global_variables_initializer())
+    # training cycle
+    for epoch in range(training_epochs):
+        avg_cost = 0
+        total_batch = int(mnist.train.num_examples / batch_size)
+
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            c, _ = sess.run([cost, optimizer], feed_dict={X: batch_xs, Y: batch_ys})
+            avg_cost += c / total_batch
+
+        print('Epoch: ', '%04d' % (epoch + 1), 'cost = ', '{:.9f}'.format(avg_cost))
+    print("Accuracy: ", accuracy.eval(session=sess,
+                                      feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
+    print("Learning Finished!")
+
+    # test the model using test sets
+    print("Accuracy: ", accuracy.eval(session=sess, feed_dict={
+        X: mnist.test.images, Y: mnist.test.labels
+    }))
+
+    # get one and predict
+    r = random.randint(0, mnist.test.num_examples - 1)
+    print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+    print("Prediction: ", sess.run(
+        tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}
+    ))
+    plt.imshow(
+        mnist.test.images[r:r + 1].reshape(28, 28),
+        cmap='Greys',
+        interpolation='nearest'
+    )
+    plt.show()
